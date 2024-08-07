5fb5c1dc0c7208eee2582a29ab8da24bb042f5ea
Danieloutliers <101993832+Danieloutliers@users.noreply.github.com>
2024-08-07 13:48:10 -0300

    n

--- outro/js/sss.js
+++ outro/js/sss.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const allowedDomain = 'seu-dominio.com';
+    const allowedDomain = 'fronteditor.dev';
     if (window.location.hostname !== allowedDomain) {
         document.body.innerHTML = '<h1>Acesso não autorizado</h1>';
     }
