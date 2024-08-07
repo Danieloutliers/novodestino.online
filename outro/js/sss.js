a3796f7970b7bd472ccce4c04e7b6e570a02d724
Danieloutliers <101993832+Danieloutliers@users.noreply.github.com>
2024-08-07 13:29:49 -0300

    d

--- outro/js/sss.js
+++ outro/js/sss.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const allowedDomain = 'seu-dominio.com';
+    const allowedDomain = 'novodestino.online';
     if (window.location.hostname !== allowedDomain) {
         document.body.innerHTML = '<h1>Acesso não autorizado</h1>';
+        setTimeout(function() {
+            window.location.href = 'plug-de-copia-de dados-invas.html';
+        }, 2000); // Redireciona após 2 segundos (2000 milissegundos)
     }
 });
