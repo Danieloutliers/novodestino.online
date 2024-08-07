d1116dc82338f13a62d9d8cf281a28cb683642db
Danieloutliers <101993832+Danieloutliers@users.noreply.github.com>
2024-08-07 13:51:49 -0300

    s

--- outr/js/sss.js
+++ outr/js/sss.js
@@ -1,6 +1,10 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const allowedDomain = 'fronteditor.dev';
     if (window.location.hostname !== allowedDomain) {
         document.body.innerHTML = '<h1>Acesso não autorizado</h1>';
+        setTimeout(function() {
+            window.location.href = 'https://www.google.com';
+        }, 2000); // Redireciona após 2 segundos (2000 milissegundos)
     }
 });
