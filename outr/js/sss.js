747bf7bd2ac48c416b8ac0bd34bb732d1285d853
Danieloutliers <101993832+Danieloutliers@users.noreply.github.com>
2024-08-07 13:59:22 -0300

    y

--- outr/js/sss.js
+++ outr/js/sss.js
@@ -1,10 +1,11 @@
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const allowedDomain = 'fronteditor.dev';
     if (window.location.hostname !== allowedDomain) {
         document.body.innerHTML = '<h1>Acesso não autorizado</h1>';
         setTimeout(function() {
-            window.location.href = 'https://www.google.com';
+            top.location.href = 'plug-de-copia-de-dados-invas.html';
         }, 2000); // Redireciona após 2 segundos (2000 milissegundos)
     }
 });
